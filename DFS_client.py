924ef10fd280265d5e32d332612e73d872a98f71
tiarnan <tiarnan>
2017-11-28 18:55:22 +0000

    needs work on caching

--- DFS_client.py
+++ DFS_client.py
@@ -1,24 +1,41 @@
-import socket                   # Import socket module
+import socket, sys, pdb # Import socket module
+
+connections = {'ServerA' : 22222,
+               'ServerB' : 22223,
+               'Locking' : 22224,
+               'Directory': 22226,
+               }
 
 s = socket.socket()             # Create a socket object
-host = socket.gethostname()     # Get local machine name
-port = 60000                    # Reserve a port for your service.
+host = socket.gethostname()# Get local machine name
+port = 22226    #This port will initially be connected to directory service
+RECV_BUFFER = 1024
+s.connect((host, port)) #set up connection, communicate with Directory
+option = s.recv(RECV_BUFFER) #received option from server 
+sys.stdout.write(option) #write this to screen
+msg = sys.stdin.readline()
+s.send(msg)
+message = s.recv(RECV_BUFFER) #this is where we receive from directory service, this won't change
+sys.stdout.write(message) #here we print the message from the Directory server
 
-s.connect((host, port))
-s.send("Hello server!")
+msg = message.split()
+file_name = msg[1]
+directory = msg[3]
+server_name = msg[5]
+File_server_port = msg[7] #later on create functions to tidy up this part
 
-with open('received_file', 'wb') as f:
-    print 'file opened'
-    while True:
-        print('receiving data...')
-        data = s.recv(1024)
-        print('data=%s', (data))
-        if not data:
-            break
-        # write data to a file
-        f.write(data)
+s.close() #close directory socket then create new one
 
-f.close()
-print('Successfully get the file')
-s.close()
-print('connection closed')
+t = socket.socket()
+PORT = connections['Locking'] #connecting to locking
+t.connect((host, PORT))
+option = t.recv(RECV_BUFFER) #received option from server 
+sys.stdout.write(option) #write this to screen
+reply = file_name
+t.send(reply)
+
+
+
+
+
+
