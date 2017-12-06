3456ea5e34dd6282140e96c997da4526251cfbd1
macgabht <macgabht@tcd.ie>
2017-12-06 01:03:47 +0000

    Caching and Version Updates needed

--- client_util.py
+++ client_util.py
@@ -1,6 +1,10 @@
-import socket, sys, pdb, Lock_ut # Import socket module
-from Lock_ut import FileLock
+import socket, sys, pdb 
 
+connections = {'ServerA' : 22222,
+               'ServerB' : 22223,
+               'Locking' : 22224,
+               'Directory': 22226,
+               }
 
 RECV_BUFFER = 1024
 
@@ -15,113 +19,104 @@ def create_socket_dir(address):
         return d
         
 
-def dir_finder(file_name, opt, s):
-    option = s.recv(RECV_BUFFER) #"what file would you like to search for?"
-    option = option.decode()
-    print (option) #write this to screen
-    print (file_name) #print out file_name, automatically send
-    data = (str(file_name) + ' ' + str(opt))
-    s.send(data.encode())
-    answer = s.recv(RECV_BUFFER) #this is where we receive from directory service, this won't change
-    answer = answer.decode()
-    return answer #should return fn, server name, server port
+def dir_finder(file_name, opt, s, JOIN_ID):
+        option = s.recv(RECV_BUFFER) #"what file would you like to search for?"
+        option = option.decode()
+        print (option) #write this to screen
+        print (file_name) #print out file_name, automatically send
+        data = (str(file_name) + ' ' + str(opt) + ' ' + str(JOIN_ID))
+        s.send(data.encode())
+        answer = s.recv(RECV_BUFFER) #this is where we receive from directory service, this won't change
+        answer = answer.decode()
+        return answer #should return fn, server name, server port
 
-def process_write(file_name, opt, host, port):
-        
-        s = create_socket_dir((host, port)) #create connection to directory, we have file_name
-        response = dir_finder(file_name, opt, s) #get answer from directory
-        #**********
-        msg = response.split()
-        file_name = msg[1]
-        server_name = msg[3]
-        file_server_port = msg[5] #have info on file, need to pass name to locking
-        print (msg)
-
-        ####skip locking for moment, connect to file server
-        PORT = int(file_server_port)
-        d = create_socket_ser((host, PORT)) #connection from....
-        print ('Create your text edit...signal the end with <DONE>.\n')
-        text_edit = ''
-        while True:
+def talk_to_locking(file_name, l, JOIN_ID, u_l): #unlock-lock
+        data = (str(u_l) + '-' + str(file_name)+'-'+str(JOIN_ID))
+        print (data)
+        l.send(data.encode())
+        reply = l.recv(RECV_BUFFER)
+        reply = reply.decode()
+        return reply
                 
+
+def write_to_fs(file_name, opt, s):
+        print ('Create your text edit...signal the end with <DONE>.\n')
+        send_edit = ''
+        while True:
                 text_edit = sys.stdin.readline()
                 if '<DONE>' in text_edit:
                     break
                 else:
                      send_edit += text_edit
-        print ('n')
-
+                     
+        print ('--------')
         message = ('FILE_NAME: ' + str(file_name) + '\nOPTION: ' + str(opt) + '\nEDIT: ' + str(send_edit)) 
         s.send(message.encode())
+        reply = s.recv(RECV_BUFFER)
+        reply = reply.decode()
+        return reply
+
+
+def read_to_fs(file_name, opt, r):
+        print ('Sending read request to File Server')
+        message = ("File_name: " +str(file_name) + '\nOption: ' + str(opt) + '\nEdit: ' + '-')
+        print (message)
+        r.send(message.encode())
+        rec = r.recv(RECV_BUFFER)
+        rec = rec.decode()
+        return rec
         
 
+def process_write(file_name, opt, host, port, JOIN_ID):
+        s = create_socket_dir((host, port)) #create connection to directory, we have file_name
+        response = dir_finder(file_name, opt, s, JOIN_ID) #get answer from directory
+        msg = response.split()
+        file_name = msg[1]
+        server_name = msg[3]
+        file_server_port = msg[5] #have info on file, need to pass name to locking
+        print (msg)
+        s.close()
+        ############################################################
+        #########LOCKING########
+        PORT = connections['Locking']
+        l = create_socket_ser((host, PORT))
+        u_l = 'lock'
+        print ('Your join_id is: ' + str(JOIN_ID))
+        print ("To gain a lock, format the string as such: lock-file_name-JOIN_ID")
+        lock_ans = talk_to_locking(file_name, l, JOIN_ID, u_l)
+        print (lock_ans)
+        l.close()
+        ##############################################################
+        #               file server                                  #
+        PORT = int(file_server_port)
+        print ('Now connecting to file server..')
+        d = create_socket_ser((host, PORT))
+        response = write_to_fs(file_name, opt, s)
+        if response == 'Write was successful.':
+               print (response)
         
-
         
-                        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-def handle_option(msg, file_name, s, port, host):
-    file_n = str(file_name)
-    if 'read' in msg:
-        with open(file_n, 'rb') as f: 
-            print('file opened')
-            while True:
-                data = s.recv(RECV_BUFFER)
-                print('receiving data to read...')
-                print(data)
-                if not data:
-                    break
                 
-        f.close()
-        print('Successfully read the file\n')
+def process_read(file_name, opt, host, port, JOIN_ID):
+        s = create_socket_dir((host, port)) #create connection to directory, we have file_name
+        response = dir_finder(file_name, opt, s, JOIN_ID) #get answer from directory
+        msg = response.split()
+        file_name = msg[1]
+        server_name = msg[3]
+        file_server_port = msg[5] #have info on file, need to pass name to locking
+        print (msg)
         s.close()
+        print (opt)
+        print ('Now connecting to file server...')
+        PORT = int(file_server_port)
+        r = create_socket_ser((host, PORT)) #connection from....
+        read_file = read_to_fs(file_name, opt, r)
+        r.close()
+        if read_file != "File could_not_be_found_here\n":
+               print (read_file)
+               
         
-    elif 'write' in msg:
-        with open(file_n, 'wb') as f: #file written to directory DFS
-            print ('file opened')
-            while True:
-                data = s.recv(RECV_BUFFER)
-                print('receiving data to write...')
-                print(data) #print data to screen
-                if not data:
-                    break
-        f.close()
-        print('Successfully read file, ready to edit')
-        s.close() #close socket and reopen it again
-        print('connection closed')
-
-        n = create_socket_ser((host, port))
-        serv = n.recv(RECV_BUFFER) #which file do you want?
-        print (serv)
-        print (file_n)
-        n.send(file_n) #send our file_name back
-        option = n.recv(RECV_BUFFER)
-        print (option)
-        opt = sys.stdin.readline()
-        n.send(opt)
-        print ('Please enter your edit to the file')
-        edit = sys.stdin.readline()
-        n.send(edit)
-        print ('edit sent')
-        n.close()
-      
-                    
-    else:
-        error = ('That was not one of the options given')
-        sys.stdout.write(error)
-        print('Failure to get files.\n')
-        s.close()
-        print('connection closed due to error')
-
-
+        
 
         
         
